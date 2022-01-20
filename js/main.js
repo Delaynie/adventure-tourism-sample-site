a16f0b16079aa3cbe656e39760dfa0869c5d995d
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-20 14:02:57 -0600

    working on getting the card to flip back

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 // activate flip card (for mobile)
 function activateFlip() {
     let inner = document.getElementsByClassName("flip-card-inner");
-    if(inner.style.transform = "rotateY(180deg") {
-        inner.style.transform = "rotateY(-180deg)";
-    } else {
-    inner.style.transform = "rotateY(180deg)";
-    }
+    
+        inner.toggleClass('active');
+    
 }
