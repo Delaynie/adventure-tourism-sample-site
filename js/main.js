6a84850fce9cae78d70f25b5e258930691ca2b94
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-21 14:30:27 -0600

    working on mobile nav design and functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 // activate flip card (for mobile)
 function activateFlip() {
     let inner = document.getElementsByClassName("flip-card-inner");
-    
-        inner.toggleClass('active');
-    
+        inner.toggleClass('active');   
 }
+
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
