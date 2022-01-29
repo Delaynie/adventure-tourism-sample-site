ff59bae59a1bede818118c189fd63b98ce06df33
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-29 10:03:53 -0600

    updating footer

--- js/main.js
+++ js/main.js
@@ -3,12 +3,6 @@
 function activateFlip() {
     let inner = document.getElementsByClassName("flip-card-inner");
       inner.toggleClass('active');
-      if(inner == 'active'){
-        inner.style.display = "none";
-      } else {
-        inner.toggleClass('active');
-      }
-    
 }
 /* Open the sidenav */
 function openNav() {
