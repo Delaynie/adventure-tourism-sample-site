94335ffa39917a9c2e7e9f12e1c36d778a461b34
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-29 09:34:05 -0600

    adjusting mobile menu dropbtn

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ function activateFlip() {
         inner.toggleClass('active');   
 }
 
+let hideBars = document.getElementById("button");
 /* Open the sidenav */
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
+    hideBars.innerHTML = "";
   }
   
   /* Close/hide the sidenav */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    hideBars.innerHTML = "|||";
   }
