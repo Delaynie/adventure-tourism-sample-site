d2384560b92cbd1bcdfcabb49106b90f585ceeea
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-29 09:39:58 -0600

    added mobile menu to other html pages, javascript update to mobile menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
+document.getElementById("button").innerHTML = "|||";
 // activate flip card (for mobile)
 function activateFlip() {
     let inner = document.getElementsByClassName("flip-card-inner");
         inner.toggleClass('active');   
 }
-
-let hideBars = document.getElementById("button");
 /* Open the sidenav */
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
-    hideBars.innerHTML = "";
+    document.getElementById("button").innerHTML = "";
   }
   
   /* Close/hide the sidenav */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    hideBars.innerHTML = "|||";
+    document.getElementById("button").innerHTML = "|||";
   }
